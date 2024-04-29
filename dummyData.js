4e0984582ed98cf17e6748be17e6c58929d0b0ad
Mariem-nad <153771066+Mariem-nad@users.noreply.github.com>
2024-04-29 22:19:09 +0200

    Add files via upload

---
+++ dummyData.js
@@ -0,0 +1,105 @@
+export const Users =[
+    {
+        id:1,
+        profilePicture: "assets/person/image.jpg",
+        username: "Safak Kocaglu",
+    },
+    {
+        id:2,
+        profilePicture: "assets/person/2.jpeg.jpg",
+        username: "Janell Shrum",
+    },
+    {   id:3,
+        profilePicture: "assets/person/3.jpeg.jpg",
+        username: "Alex Durden",
+
+    },
+    {
+        id:4,
+        profilePicture:"assets/person/4.jpg.webp",
+        username: "Dora Hawks",
+    },
+    {
+        id:5,
+        profilePicture: "assets/person/5.jpg.webp",
+        username: "Thomas Holden",
+    },
+    {
+       id:6,
+       profilePicture: "assets/person/6.jpg.webp",
+       username: "Shirley Beauchamp", 
+    },
+    {
+        id:7,
+        profilePicture: "assets/person/7.jpg.webp",
+        username: "Trvis Bennett",
+    },
+    {
+        id:8,
+        profilePicture: "assets/person/8.jpg.webp",
+        username: "Kristen Thomas",
+    },
+    {
+        id:9,
+        profilePicture: "assets/person/9.jpg.webp",
+        username: "Gary Duty"
+    },
+    {
+        id:10,
+        profilePicture: "assets/person/10.jpeg.jpg",
+        username: "Safak Kocaglu",
+    },
+];
+ 
+export const Posts =[
+    {
+        id:1,
+        desc:"Love For All, Hatred For None.",
+        photo:"assets/post/1.image.jpg.webp",
+        date: "5 mins ago",
+        userId: 1,
+        like: 32,
+        comment: 9,    
+    },
+    {
+        id:2,
+        photo:"assets/post/2.image.jpg.webp",
+        date: "5 mins ago",
+        userId: 2,
+        like: 2,
+        comment: 1,      
+    },
+    {
+        id:3,
+        desc: "Every moment is a fresh beginning",
+        photo:"assets/post/3.img.jpg.jpg",
+        date: "1 hour ago",
+        userId: 3,
+        like: 61,
+        comment: 2,      
+    },
+    {
+      id:4,
+      photo:"assets/post/4.image.jpg.webp",
+        date: "4 hour ago",
+        userId: 4,
+        like: 7,
+        comment: 3,       
+    },
+    {
+        id:5,
+        photo:"assets/post/5.imag.jpg.webp",
+        date: "5 hour ago",
+        userId: 5,
+        like: 23,
+        comment: 5,      
+    },
+    {
+        id:6,
+        photo:"assets/post/6.pjg.webp",
+        date: "6 hour ago",
+        userId: 6,
+        like:25,
+        comment: 6,      
+    }
+]
